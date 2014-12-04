17cc4d50620c212931e70611d9036dd2e7bad60f
blue4world <blue4world@gmail.com>
2014-12-04 14:38:58 +0100

    added ChartFrame class

---
+++ Canteen/src/canteen/graph/ChartFrame.java
@@ -0,0 +1,118 @@
+package canteen.graph;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+
+import canteen.result.Result;
+
+public class ChartFrame extends ApplicationFrame {
+	ArrayList<Result> results;
+	ArrayList<Double> waitingTimes;
+
+	public ChartFrame(final String title, ArrayList<Result> results, ArrayList<Double> waitingTimes) throws Exception {
+		super(title);
+		this.results = results;
+		this.waitingTimes = waitingTimes;
+
+		if (results.size() != waitingTimes.size()) {
+			throw new Exception("Results and WaitingTimes have got different sizes");
+		}
+
+		final XYDataset dataset = createDataset();
+		final JFreeChart chart = createChart(dataset);
+		final ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+		setContentPane(chartPanel);
+
+	}
+
+	private XYDataset createDataset() {
+		// foodQueue
+		XYSeries foodQueue = new XYSeries("foodQueue");
+		for (Result result : results) {
+			foodQueue.add((double)result.getTime(), (double)result.getNumberOfPeopleInFoodQueue());
+		}
+
+		// tableQueue
+		XYSeries tableQueue = new XYSeries("tableQueue");
+		for (Result result : results) {
+			foodQueue.add((double)result.getTime(), (double)result.getNumberOfPeopleInTableQueue());
+		}
+
+		// eating
+		XYSeries eating = new XYSeries("eating");
+		for (Result result : results) {
+			foodQueue.add((double)result.getTime(), (double)result.getNumberOfPeopleEating());
+		}
+
+		// waitingTime
+		XYSeries waitingTime = new XYSeries("waitingTime");
+		int i = 0;
+		for (Result result : results) {
+			foodQueue.add((double)result.getTime(), (double)waitingTimes.get(i));
+			i++;
+		}
+
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(foodQueue);
+		dataset.addSeries(tableQueue);
+		dataset.addSeries(eating);
+		dataset.addSeries(waitingTime);
+
+		return dataset;
+	}
+
+    private JFreeChart createChart(final XYDataset dataset) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            "Line Chart Demo 6",      // chart title
+            "X",                      // x axis label
+            "Y",                      // y axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL,
+            true,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+        chart.setBackgroundPaint(Color.white);
+
+//        final StandardLegend legend = (StandardLegend) chart.getLegend();
+  //      legend.setDisplaySeriesShapes(true);
+        
+        // get a reference to the plot for further customisation...
+        final XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+    //    plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesShapesVisible(1, false);
+        plot.setRenderer(renderer);
+
+        // change the auto tick unit selection to integer units only...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        // OPTIONAL CUSTOMISATION COMPLETED.
+                
+        return chart;
+        
+    }
+
+}
